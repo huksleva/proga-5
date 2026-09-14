1806148ef040f744751ffcf9482efdc3bf8b51da
Leonid <leonid005xc@gmail.com>
2026-09-14 20:46:28 +0300

    Комит

--- ЛР1/examples/test_github_pages.py
+++ ЛР1/examples/test_github_pages.py
@@ -3,16 +3,15 @@ import sys
 from remote_import import url_hook
 
 
-REMOTE_URL = "https://github.com/huksleva/proga-5"
+REMOTE_URL = "https://huksleva.github.io/proga-5/"
 
-
-sys.path_hooks.append(url_hook)
-sys.path.append(REMOTE_URL)
+sys.path_hooks.insert(0, url_hook)
+sys.path.insert(0, REMOTE_URL)
 sys.path_importer_cache.clear()
 
+print("sys.path:", sys.path[:3])
 
 import remote_package
-
 
 print(remote_package.add(10, 20))
 print(remote_package.multiply(6, 7))
